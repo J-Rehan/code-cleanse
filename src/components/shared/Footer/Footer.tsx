9fd88615f01e120423a616a9aa964f260f7f086d
zeek <zeektate7@gmail.com>
2023-03-10 23:46:35 +0545

    fix: add well-known

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer text-white p-8">
+    <footer className="footer text-white p-8 pb-32">
       <div className="max-w-[1024px] mx-auto md:flex justify-between ">
         <Link href="/">
           <Image
