3bfa4cd1c7d23237c4b704d32470f6824750e603
zeek <zeektate7@gmail.com>
2023-03-13 23:46:08 +0545

    fix: update clarity code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,7 @@ const App: React.FC<AppProps> = (props) => {
       <Script id="clarity">
         {`
           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "fvqwj82efx");
