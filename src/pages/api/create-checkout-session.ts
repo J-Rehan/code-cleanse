d6445ff30e9618c886b5f213560f4603beaa4d05
zeek <zeektate7@gmail.com>
2023-10-16 14:17:18 +0545

    fix: patch update

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -1,12 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import Stripe from 'stripe'
-import * as bizSdk from 'facebook-nodejs-business-sdk'
-import * as admin from 'firebase-admin'
+import { NextApiRequest, NextApiResponse } from 'next';
+import Stripe from 'stripe';
+import * as admin from 'firebase-admin';
 
-const domainUrl = process.env.DOMAIN_URL || 'https://www.codecleanse.com'
+const domainUrl = process.env.DOMAIN_URL || 'https://www.codecleanse.com';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15',
-})
+});
 
 const subscriptions = {
   OneTime: {
@@ -49,69 +48,36 @@ const subscriptions = {
           : 'price_1MlEc0Ew4G60H813ZXaqJqF0',
     },
   },
-}
+};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
-    return res.status(405).end()
+    return res.status(405).end();
   }
   try {
     const { name, email, subscriptionType, firebaseUserId } = req.body as {
-      name: string
-      email: string
-      subscriptionType: keyof typeof subscriptions
-      firebaseUserId: string
-    }
+      name: string;
+      email: string;
+      subscriptionType: keyof typeof subscriptions;
+      firebaseUserId: string;
+    };
 
-    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK as string)
+    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK as string);
     if (!admin.apps.length) {
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-      })
+      });
     }
 
     const collectionName =
-      process.env.NODE_ENV === 'production' ? 'users' : 'dev-users'
+      process.env.NODE_ENV === 'production' ? 'users' : 'dev-users';
 
     const docRes = await admin
       .firestore()
       .collection(collectionName)
       .doc(firebaseUserId)
-      .get()
-    const doc = docRes.data()
-
-    const { EventRequest, UserData, ServerEvent } = bizSdk
-    const fbConversionAccessToken = process.env.FB_CONVERSION_ACCESS_TOKEN
-    const pixelId = process.env.FB_PIXEL_ID
-
-    bizSdk.FacebookAdsApi.init(fbConversionAccessToken)
-    let currentTimestamp = Math.floor(new Date().getTime() / 1000)
-    const userData = new UserData()
-      .setLastName(name.split(' ')[0])
-      .setClientUserAgent(req.headers['user-agent'])
-      .setEmail(email)
-      .setFirstName(name.split(' ')[1])
-      .setClientIpAddress(
-        req.socket.remoteAddress || req.connection.remoteAddress,
-      )
-      .setPhone(doc.phone)
-      .setFbp(`fb.1.${currentTimestamp}.1098115397`)
-      .setFbc(`fb.1.${currentTimestamp}.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890`)
-
-    const serverEvent = new ServerEvent()
-      .setEventTime(currentTimestamp)
-      .setEventName('CompleteRegistration')
-      .setEventSourceUrl('https://www.codecleanse.com')
-      .setActionSource('website')
-      .setEventId('CompleteRegistration')
-      .setUserData(userData)
-
-    const eventsData = [serverEvent]
-    const eventRequest = new EventRequest(
-      fbConversionAccessToken,
-      pixelId,
-    ).setEvents(eventsData)
-    await eventRequest.execute()
+      .get();
+    const doc = docRes.data();
 
     if (
       !['Monthly', 'OneTime', 'Annual'].includes(subscriptionType) ||
@@ -121,12 +87,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(400).send({
         success: false,
         message: 'Insufficient Parameters',
-      })
+      });
     }
 
     const price = await stripe.prices.retrieve(
       subscriptions[subscriptionType].prices.default,
-    )
+    );
 
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
@@ -148,20 +114,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         name,
       )}&email=${encodeURIComponent(
         email,
-      )}&subscriptionType=${subscriptionType}&price=${price.unit_amount
-        }&firebaseUserId=${firebaseUserId}`,
+      )}&subscriptionType=${subscriptionType}&price=${
+        price.unit_amount
+      }&firebaseUserId=${firebaseUserId}`,
       cancel_url: `${domainUrl}/hire-cancel`,
-    })
+    });
 
-    return res.redirect(303, session.url)
+    return res.redirect(303, session.url);
   } catch (e) {
-    res.status(400)
+    res.status(400);
     return res.send({
       error: {
         message: e.message,
       },
-    })
+    });
   }
-}
+};
 
-export default handler
+export default handler;
